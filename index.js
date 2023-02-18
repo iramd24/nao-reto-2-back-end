48b5b5640784e9d24d2b1726eb981ed0bde664b8
iramd24 <idr.diaz96@gmail.com>
2023-02-17 19:45:55 -0600

    changed cors options
    changed cors options

--- index.js
+++ index.js
@@ -14,26 +14,15 @@ import capacityMessage from './models/capacityMessage.js';
 
 const app = express();
 
-let corsOrigins=[];
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  });
-//app.use(cors(corsOptions));
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
 app.use(bodyparser.json({limit: "3mb", extended:"true"}));
 app.use(bodyparser.urlencoded({limit: "3mb", extended:"true"}));
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const CONNECTION_URL = 'mongodb+srv://naodbadmin:naodbadmin123@cluster0.fgokoxi.mongodb.net/?retryWrites=true&w=majority';
